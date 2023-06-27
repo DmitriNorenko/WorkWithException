a105b50174a86b26e9661de3784a6a9a11e6366b
Name <email@exaple.ru>
2023-06-27 08:39:41 +0300

    9.3.12

--- WorkWithException/Program.cs
+++ WorkWithException/Program.cs
@@ -8,33 +8,15 @@ namespace WorkWithException
 {
     internal class Program
     {
+        delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            Action showMessageDelegate = ShowMessage;
-            showMessageDelegate.Invoke();
-
-            Func<int, int, int, int> sumDelegate = Sum;
-            int result = sumDelegate.Invoke(1, 30, 120);
-            Console.WriteLine(result);
-
-            Predicate<string> checkLengthDelegate = CheckLength;
-            bool status = checkLengthDelegate.Invoke("skill_factory");
-            Console.WriteLine(status);
-        }
-        public static void ShowMessage()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-        public static int Sum(int a, int b, int c)
-        {
-            return a + b + c;
-        }
-
-        public static bool CheckLength(string _row)
-        {
-            if (_row.Length > 3) return true;
-            return false;
+            ShowMessageDelegate showMessageDelegate = delegate (string message)
+            {
+                Console.WriteLine(message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
         }
     }
 }
