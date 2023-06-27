25d8bfa8e86ce79375bf07c3a0eecbefaba16d94
Name <email@exaple.ru>
2023-06-27 09:09:22 +0300

    9.3.15

--- WorkWithException/Program.cs
+++ WorkWithException/Program.cs
@@ -9,11 +9,12 @@ namespace WorkWithException
 {
     internal class Program
     {
-        delegate void ShowMessageDelegate(string _message);
+        delegate int RandomNumberDelegate();
         static void Main(string[] args)
         {
-            ShowMessageDelegate showMessageDelegate = (string message) => Console.WriteLine(message);
-            showMessageDelegate.Invoke("Hello World!");
+            RandomNumberDelegate randomNumberDelegate = () => new Random().Next(0, 100);
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
             Console.Read();
         }
     }
